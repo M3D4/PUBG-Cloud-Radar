7e5bd60be11b9643c79ec7c62ccf9429c101a21a
M3D4 <ehy@qq.com>
2018-05-03 20:03:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ Example: node index.js playback '/SOME/COOL/DIR/pubg-game1.pcap' | pino
 }
 
 function startWebServer () {
-  const apiServerPort = 20086
+  const apiServerPort = 8999
   backend.listen(apiServerPort, () => {
     console.log('Scientific Chicken Dinner listening on http://localhost:' + apiServerPort)
   })
